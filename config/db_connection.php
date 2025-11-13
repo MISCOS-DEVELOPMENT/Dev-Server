cb4200a13d9fb235e109a054c3d9f780c1123868
MISCOS-DEVELOPMENT <git.dev@miscos.in>
2025-11-13 13:55:13 +0530

    Update db_connection.php

--- config/db_connection.php
+++ config/db_connection.php
@@ -14,7 +14,7 @@ final class DB
             $port = getenv('DB_PORT') ?: '3306';
             $name = getenv('DB_NAME') ?: 'dev_geeta';
             $user = getenv('DB_USER') ?: 'dev_miscos';
-            $pass = getenv('DB_PASS') ?: 'x!^(mEBad1r YSsvA';
+            $pass = getenv('DB_PASS') ?: 'x!^(mEBad1rYSsvA';
             
             // For Azure Database - SSL is REQUIRED
             $this->conn = new \mysqli();
